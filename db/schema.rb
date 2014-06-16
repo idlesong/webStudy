09239e5ecdcadf9be92714baddf0ae1c3fde17e1
idlesong <idlesong@gmail.com>
2014-06-16 11:31:47 +0800

    add readme.md file

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130703020825) do
 
-  create_table "catalogs", :force => true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.decimal  "level"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
   create_table "customers", :force => true do |t|
     t.string   "name"
     t.text     "adress"
---
+++ readme.md
@@ -0,0 +1,21 @@
+# web study(Web excises)
+Web excises, current focused on rails.
+
+## Goal
+- 各种教程的实现，尽量完善（实现几种常用形式，并有来源）
+- 能够快速预览，并能清晰看到各种实现（配截图和简介）
+
+## demos
+#### Home
+- lessons
+- 可以使用table的方式，或者卡片式的预览方式（配截图和overview）
+
+#### navigation bar & layouts
+
+#### Ajax CRUD form
+- 参考 Chrome的 search engines 设置界面
+
+
+#### Popup table
+
+#### Google now style
